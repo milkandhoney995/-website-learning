6c9e2ecefd84f700c29922e5962a125f2f3dbea6
cri-harada.utano <harada.utano@cri-mw.co.jp>
2023-02-16 15:33:55 +0900

    menuListをnavigationコンポーネント内に移動

--- trace/2023/Personal HP/sample01/components/layouts/navigation/navigation.tsx
+++ trace/2023/Personal HP/sample01/components/layouts/navigation/navigation.tsx
@@ -1,12 +1,20 @@
 import classes from "./navigation.module.scss";
 import Link from "next/link";
 
+
+const menuList = [
+  { id: 1, href: "/profile", title: "PROFILE" },
+  { id: 2, href: "/new", title: "NEW WORKS" },
+  { id: 3, href: "/works", title: "WORKS" },
+  { id: 4, href: "/gallery", title: "GALLERY" },
+];
+
 export default function Navigation(props) {
   return(
     <nav className={`${classes.nav} ${props.showMenu ? `${classes.nav__activeNav}` : ""}`}>
       <div className={classes.nav__content}>
         <ul className={classes.nav__menu}>
-          {props.menus.map((menu) => (
+          {menuList.map((menu) => (
             <li key={menu.id}>
               <Link href={menu.href} onClick={props.onClick}>
                   {menu.title}
